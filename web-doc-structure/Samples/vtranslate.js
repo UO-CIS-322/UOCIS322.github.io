d93ec1d452556c597f771e85c5038bcca4e000bd
Michal Young <michal.young@gmail.com>
2019-04-08 10:12:42 -0700

    Removed JQuery dependence, now using so-called modern DOM API

--- web-doc-structure/Samples/vtranslate.js
+++ web-doc-structure/Samples/vtranslate.js
@@ -9,20 +9,45 @@
 // Installation check
 
 function is_it_installed() {
-    console.log("It is installed");
+     console.log("It is installed");
 }
 
-function next_english_toggle() {
+// JQuery version: 
+// function next_english_toggle() {
+//     console.log("Click noted");
+//     tr = $(this).next(".en");
+//     if (tr == null) {
+//         console.log("Didn't find translation");
+//         return;
+//     }
+//     tr.toggleClass("reveal");
+// }
+
+// "Modern" DOM API version:
+function toggle_translation(event) {
     console.log("Click noted");
-    tr = $(this).next(".en");
-    if (tr == null) {
-        console.log("Didn't find translation");
-        return;
+    // The 'this' object is set automatically in event handlers
+    var sib = this.nextElementSibling;
+    // The very next element should be the english translation,
+    // but we'll search for it just in case. 
+    while (sib) {
+	console.log("Checking " + sib); 
+	css_classes = sib.classList;
+	if (css_classes.contains("en")) {
+	    css_classes.toggle("reveal");
+	    console.log("Toggled!");
+	    return;
+	}
+	sib = sib.nextElementSibling; 
     }
-    tr.toggleClass("reveal");
+    alert("Didn't find translation");
+    return;
 }
 
-$(document).ready( function() {
-    $(".es").click(next_english_toggle);
-});
 
+document.addEventListener("DOMContentLoaded", function() {  
+    let es_elements = document.querySelectorAll(".es");
+    for (let i=0; i < es_elements.length; ++i) {
+	es_elements[i].addEventListener('click', toggle_translation);
+    }
+});
