0d59f45be0c3d1a125a7d98158f7146056d3e50e
Michal Young <michal@cs.uoregon.edu>
2019-04-19 16:18:47 -0700

    Cleaned out cruft from earlier versions

--- web-doc-structure/Samples/vtranslate.js
+++ web-doc-structure/Samples/vtranslate.js
@@ -1,29 +1,7 @@
-/* Here: 
- * Javascript (using jquery) that turns individual lines of 
- * English translation "on" and "off" by inserting or removing 
- * the "reveal" class on span.  We listen for click on a spanish
- * line, and turn the following English span into class="en" or 
- * class="en reveal", toggling it. 
- */
-
-// Installation check
-
-function is_it_installed() {
-     console.log("It is installed");
-}
-
-// JQuery version: 
-// function next_english_toggle() {
-//     console.log("Click noted");
-//     tr = $(this).next(".en");
-//     if (tr == null) {
-//         console.log("Didn't find translation");
-//         return;
-//     }
-//     tr.toggleClass("reveal");
-// }
-
-// "Modern" DOM API version:
+// vtranslate: 
+//   Demonstration of modifying element visibility 
+//   by handling key clicks in javascript. 
+// 
 function toggle_translation(event) {
     console.log("Click noted");
     // The 'this' object is set automatically in event handlers
