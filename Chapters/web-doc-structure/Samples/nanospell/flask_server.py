6dc07867ac2b329c0b3fc3467cc2e7c8277058a6
Michal Young <michal.young@gmail.com>
2020-05-23 20:50:08 -0700

    Converting from gitbook to Github pages, work in progress

---
+++ Chapters/web-doc-structure/Samples/nanospell/flask_server.py
@@ -0,0 +1,134 @@
+"""
+Tiny server provides spelling completion suggestions on port 5678.
+Constructed using the flask web application framework for Python.
+"""
+import config
+
+import flask
+from flask import request
+from flask import jsonify
+import zipfile
+import logging
+
+from typing import List
+
+###
+# Globals
+###
+app = flask.Flask(__name__)
+WORDLIST = []   # Loaded on first request
+
+import uuid
+
+app.secret_key = str(uuid.uuid4())
+app.debug = config.DEBUG
+app.logger.setLevel(logging.DEBUG)
+
+##############
+# The page on which we will get spelling
+# suggestions.  To avoid some security complications,
+# the page and the Ajax response come from the same
+# server.
+###############
+
+@app.route("/")
+def nanospell():
+    return flask.render_template("nanospell.html")
+
+
+###############
+#
+# AJAX request handlers
+#   These return JSON, rather than rendering pages.
+#
+###############
+@app.route("/_suggest_completions")
+def suggest_completions():
+    """
+    Up to k suggestions for completing a word
+    """
+    app.logger.debug("Got a JSON request")
+    prefix = request.args.get('prefix', "default", type=str)
+    app.logger.debug(f"Prefix: '{prefix}")
+    app.logger.debug(f"The request object: {request}")
+    app.logger.debug(f"The arguments: '{request.args}")
+    if prefix:
+        app.logger.debug(f"Looking up '{prefix}' in {len(WORDLIST)} words")
+        completions = get_completions(prefix, 5)
+        app.logger.debug(f"Found completions {completions}")
+    else:
+        app.logger.debug("Didn't have a prefix to look up")
+        completions = []
+    result = jsonify(suggestions=completions)
+    return result
+
+
+#############
+# Used by request handlers
+#############
+
+def load_wordlist():
+    """Load the WORDLIST from an external file"""
+    global WORDLIST
+    zipped = zipfile.ZipFile(config.WORDFILE)
+    reader = zipped.open("dict.txt")
+    words = [line.decode('utf8').strip() for line in reader]
+    # Note we could have a benign race condition:  If the first
+    # two requests come in very close to each other, we will read
+    # word list twice (or more), and build unnecessary copies of the
+    # list, but as assignment to the list is atomic we will still end
+    # up with one valid copy of the list.
+    WORDLIST = words
+
+
+def get_completions(prefix: str, max_completions: 5) -> List[str]:
+    global WORDLIST
+    if not WORDLIST: 
+        load_wordlist()
+    # Standard binary search until we succeed or fail; then
+    # whether we succeed or fail, the next few words could be
+    # completions.
+    low = 0
+    high = len(WORDLIST) - 1
+    while high >= low:
+        probe = (high + low) // 2
+        probe_word = WORDLIST[probe]
+        if probe_word == prefix:
+            return scan_completions(prefix, WORDLIST, probe, max_completions)
+        if probe_word > prefix:
+            high = probe - 1
+        else:
+            low = probe + 1
+    # If we completed the search without finding a match,
+    # then 'low' is the index of the first word that could potentially
+    # be a match.
+    #
+    return scan_completions(prefix, WORDLIST, low, max_completions)
+
+
+def scan_completions(prefix, words, start_scan, max_entries=5) -> List[str]:
+    """Starting at words[start_scan], return up to max_entries strings
+    that could be completions of prefix.
+    """
+    completions = []
+    count = 0
+    probe = start_scan
+    while count <= max_entries and probe < len(words) and words[probe].startswith(prefix):
+        completions.append(words[probe])
+        probe += 1
+        count += 1
+    return completions
+
+
+#############
+
+
+if __name__ == "__main__":
+    import uuid
+
+    app.secret_key = str(uuid.uuid4())
+    app.debug = config.DEBUG
+    app.logger.setLevel(logging.DEBUG)
+    app.run(port=config.PORT)
+
+
---
+++ index.markdown
@@ -0,0 +1,18 @@
+---
+# Feel free to add content and custom Front Matter to this file.
+# To modify the layout, see https://jekyllrb.com/docs/themes/#overriding-theme-defaults
+
+layout: home
+title: Introduction to Software Engineering
+---
+
+# Chapters
+
+* [Introduction](README.md)
+* [HTML+CSS+JS: The parts of a web page](Chapters/web-doc-structure/README.html)
+  * [HTML for structure and content](Chapters/web-doc-structure/html.html)
+  * [Cascading style sheets for presentation](Chapters/web-doc-structure/css.html)
+  * [Javascript for interaction](Chapters/web-doc-structure/js.html)
+* [Architecture of the Web](Chapters/architecture-of-the-web.md)
+
+
